b7e4bb2ec3f0bf5cb975e8d0c00e528989cf7070
Daniele Del Piano <daniele.delpiano30@gmail.com>
2022-08-31 18:51:46 +0100

    1.6 added a test class

--- app/src/androidTest/java/com/example/simpleweatherapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/simpleweatherapp/ExampleInstrumentedTest.kt
@@ -1,12 +1,23 @@
 package com.example.simpleweatherapp
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.simpleweatherapp.view.MainActivity
+import org.hamcrest.Matchers.containsString
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +26,22 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.simpleweatherapp", appContext.packageName)
+    fun getCityweather() {
+        onView(withId(R.id.txt_insert_city))
+            .perform(typeText("Napoli"))
+            .perform(ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.btn_search_city))
+            .perform(click())
+
+        onView(withId(R.id.weather_main))
+            .check(matches(withText(containsString("Naples"))))
     }
+
+
+
+
 }

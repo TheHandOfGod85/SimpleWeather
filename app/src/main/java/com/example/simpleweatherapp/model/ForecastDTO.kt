c23b54770d0f9e214b4ad228568c308b9ecba40d
Daniele Del Piano <daniele.delpiano30@gmail.com>
2022-09-01 20:04:25 +0100

    1.7 added a new feature to get the forecast

--- app/src/main/java/com/example/simpleweatherapp/model/ForecastDTO.kt
+++ app/src/main/java/com/example/simpleweatherapp/model/ForecastDTO.kt
@@ -3,7 +3,7 @@ package com.example.simpleweatherapp.model
 
 import com.google.gson.annotations.SerializedName
 
-// new
+// new gfgf
 data class ForecastDTO (
 
     @SerializedName("cod"     ) var cod     : String?         = null,
